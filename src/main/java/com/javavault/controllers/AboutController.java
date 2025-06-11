f9f7ab0eeba86aeafa4975bd6a3c260e64e89f9d
Tyler Shingler <102036859+TAShingler@users.noreply.github.com>
2025-06-11 00:09:03 -0400

    Added 'About' view

---
+++ src/main/java/com/javavault/controllers/AboutController.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.javavault.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author tyler
+ */
+public class AboutController implements Initializable {
+    
+    //<editor-fold defaultstate="collapsed" desc=" Fields/Properties ">
+    
+    
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc=" UI Controls ">
+    
+    
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc=" Constructors ">
+    
+    
+    
+    //</editor-fold>
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    //<editor-fold defaultstate="collapsed" desc=" Accessors (get) ">
+    
+    
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc=" Mutators (set) ">
+    
+    
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc=" Event Handlers ">
+    
+    
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc=" Other Methods ">
+    
+    
+    
+    //</editor-fold>
+    
+}
---
+++ src/main/java/com/javavault/viewmodels/AboutViewModel.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.javavault.viewmodels;
+
+/**
+ *
+ * @author tyler
+ */
+public class AboutViewModel extends ViewModelBase {
+    
+    //<editor-fold defaultstate="collapsed" desc=" Fields/Properties ">
+    
+    
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc=" Constructors ">
+    
+    
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc=" Accessors (get) ">
+    
+    
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc=" Mutators (set) ">
+    
+    
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc=" Event Handlers ">
+    
+    
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc=" Other Methods ">
+    
+    
+    
+    //</editor-fold>
+    
+}
